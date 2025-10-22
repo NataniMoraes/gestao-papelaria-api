59a10387b7fad55d402d7fb4671bf737ce7e9472
NataniMoraes <natanigabriele@gmail.com>
2025-10-22 20:20:05 -0300

    Semana 1
    Semana 1

---
+++ src/main/java/br/com/papelaria/gestao_papelaria/GestaoPapelariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.papelaria.gestao_papelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPapelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPapelariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/papelaria/gestao_papelaria/GestaoPapelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.papelaria.gestao_papelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPapelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
