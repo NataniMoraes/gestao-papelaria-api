39c80235a20ab6f14e1f0d5a37d808b123fe7311
NataniMoraes <natanigabriele@gmail.com>
2025-11-10 22:06:41 -0300

    Create WebConfig.java

---
+++ src/main/java/br/com/papelaria/gestao_papelaria/config/WebConfig.java
@@ -0,0 +1,19 @@
+package br.com.papelaria.gestao_papelaria.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todos os endpoints ("/**")
+                .allowedOrigins("http://localhost:5173") // Permite requisições SOMENTE desta origem
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
