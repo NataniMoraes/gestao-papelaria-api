16966a48e2b48ac27d55ac053f274c15f1ee680d
NataniMoraes <natanigabriele@gmail.com>
2025-11-11 22:21:23 -0300

    Create DashboardStatsDTO.java

---
+++ src/main/java/br/com/papelaria/gestao_papelaria/dto/DashboardStatsDTO.java
@@ -0,0 +1,14 @@
+package br.com.papelaria.gestao_papelaria.dto;
+
+// Para o Dash da versão web
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class DashboardStatsDTO {
+    private long totalProdutos;
+    private long totalCategorias;
+}
