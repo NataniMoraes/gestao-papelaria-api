38be22fa33ef36aac1f9ee728f03e7688c743369
NataniMoraes <natanigabriele@gmail.com>
2025-11-17 21:04:11 -0300

    Update VendaRequestDTO.java

--- src/main/java/br/com/papelaria/gestao_papelaria/dto/VendaRequestDTO.java
+++ src/main/java/br/com/papelaria/gestao_papelaria/dto/VendaRequestDTO.java
@@ -2,9 +2,13 @@ package br.com.papelaria.gestao_papelaria.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 public class VendaRequestDTO {
     private List<ItemVendaDTO> itens;
+
+    private String formaPagamento;
+    private BigDecimal desconto;
 }
